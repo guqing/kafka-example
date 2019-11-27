6247bb36e965e161516b55f89646ad34fee94663
guqing <1484563614@qq.com>
2019-11-27 19:08:53 +0800

    init commit

---
+++ src/main/java/xyz/guqing/kafka/KafkaApplication.java
@@ -0,0 +1,17 @@
+package xyz.guqing.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guqing
+ * @date 2019-11-27 17:16
+ */
+@SpringBootApplication
+public class KafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaApplication.class, args);
+    }
+
+}
